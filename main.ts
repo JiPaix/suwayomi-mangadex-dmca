a2466257aa02a4f0e88e958a5af802a072625e47
Jean-Philippe ALLEGRO <26584973+JiPaix@users.noreply.github.com>
2025-06-02 01:05:38 +0200

    perf: reduce number of queries
    Co-authored-by: Robonau <30987265+Robonau@users.noreply.github>

--- main.ts
+++ main.ts
@@ -76,56 +76,39 @@ async function fetchGraphQL<T = any>(payload: any): Promise<T> {
   return json.data;
 }
 
-async function fetchAllCategories(): Promise<{catId: number, catName: string}[]> {
+async function fetchAllMangas(): Promise<{id:number, title: string, source: string, realUrl: string, categories: string[], status: string}[]> {
   const data = await fetchGraphQL({
-    operationName: "GET_CATEGORIES_LIBRARY",
+    operationName: "GET_MANGAS",
     variables: { order: [{ by: "ORDER" }] },
-    query: `query GET_CATEGORIES_LIBRARY($order: [CategoryOrderInput!]) {
-      categories(order: $order) {
-        nodes { id name }
+    query: `query GET_MANGAS {
+      mangas(condition: {inLibrary: true, sourceId: "2499283573021220255"}) {
+        nodes {
+          id
+          title
+          source {
+            displayName
+          }
+          status
+          realUrl
+          categories {
+            nodes {
+              name
+            }
+          }
+        }
       }
     }`
   });
 
-  return data.categories.nodes.map((c: any) => ({catId: c.id, catName: c.name}));
-}
+  return data.mangas.nodes.map((v: {id: number, status: string, title: string, source: {displayName: string}, realUrl: string,  categories: {nodes: {name: string}[]}}) => ({
+    id: v.id,
+    title: v.title,
+    source: v.source.displayName,
+    realUrl: v.realUrl,
+    categories: v.categories.nodes.length ? v.categories.nodes.map((n:{name: string}) => n.name) : null,
+    status: v.status
+  }))
 
-async function fetchAllMangasForCategory(categoryId: number): Promise<MangaEntry[]> {
-  const data = await fetchGraphQL({
-    operationName: "GET_CATEGORY_MANGAS",
-    variables: { id: categoryId },
-    query: `query GET_CATEGORY_MANGAS($id: Int!) {
-      category(id: $id) {
-        mangas {
-          nodes {
-            id title
-            source {
-              displayName
-            }
-            status
-            realUrl
-          }
-        }
-      }
-    }`,
-  });
-
-  return data.category.mangas.nodes;
-}
-
-async function getAllLibraryMangas(): Promise<MangaEntry[]> {
-  const categories = await fetchAllCategories();
-  const results: MangaEntry[] = [];
-
-  for (const category of categories) {
-    const mangas = await fetchAllMangasForCategory(category.catId);
-
-    results.push(...mangas.map(m => {
-      m.category = category.catName
-      return m
-    }));
-  }
-  return results;
 }
 
 // FETCH AND PARSE GDOC
@@ -162,15 +145,14 @@ async function main() {
     printError(e, {text: "Google Sheet", position: 0})
     Deno.exit(1);
   });
-  const local = await getAllLibraryMangas().catch(e => {
+  const local = await fetchAllMangas().catch(e => {
     printError(e, {text: "Suwayomi Instance", position: 0})
     Deno.exit(1);
   });
 
   const striked = local.filter(v => 
-    v.source.displayName === "MangaDex (EN)" &&
     gSheet.some(x => v.realUrl.includes(x.uuid))
-  ).map(v => ({title: v.title, category: v.category, status: v.status, url: `${SUWAYOMI.origin}/manga/${v.id}`}))
+  ).map(v => ({title: v.title, categories: v.categories, status: v.status, url: `${SUWAYOMI.origin}/manga/${v.id}`}))
 
   console.table(striked);
 }
